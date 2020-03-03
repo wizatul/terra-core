401df3cd44e459c9aa1de6a6899ca83455c5b3db
Stephen Esser <Stephen.Esser@Cerner.com>
2020-03-03 16:40:13 -0600

    Stop event propagation

--- packages/terra-form-select/src/search/Menu.jsx
+++ packages/terra-form-select/src/search/Menu.jsx
@@ -267,7 +267,7 @@ class Menu extends React.Component {
           isCheckable: false,
           isSelected: MenuUtil.isSelected(this.props.value, option.props.value),
           variant: 'search',
-          onMouseDown: (event) => { console.log('[Search Menu] - Option - OnMouseDown'); event.preventDefault(); this.downOption = option; },
+          onMouseDown: (event) => { console.log('[Search Menu] - Option - OnMouseDown'); event.preventDefault(); event.stopPropagation(); this.downOption = option; },
           onMouseUp: event => this.handleOptionClick(event, option),
           onMouseEnter: event => this.handleMouseEnter(event, option),
           ...(option.props.value === this.state.active) && { 'data-select-active': true },
