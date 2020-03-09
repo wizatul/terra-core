a5ef83cd376e482778cc7e212b1be29d39e1633a
Stephen Esser <Stephen.Esser@Cerner.com>
2020-03-09 12:29:15 -0500

    Stop propagation on option mouse down

--- packages/terra-form-select/src/search/Menu.jsx
+++ packages/terra-form-select/src/search/Menu.jsx
@@ -261,7 +261,11 @@ class Menu extends React.Component {
           isCheckable: false,
           isSelected: MenuUtil.isSelected(this.props.value, option.props.value),
           variant: 'search',
-          onMouseDown: (event) => { event.preventDefault(); this.downOption = option; },
+          onMouseDown: (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            this.downOption = option;
+          },
           onMouseUp: event => this.handleOptionClick(event, option),
           onMouseEnter: event => this.handleMouseEnter(event, option),
           ...(option.props.value === this.state.active) && { 'data-select-active': true },
--- packages/terra-form-select/src/tag/Menu.jsx
+++ packages/terra-form-select/src/tag/Menu.jsx
@@ -277,7 +277,11 @@ class Menu extends React.Component {
           isCheckable: true,
           isSelected: MenuUtil.isSelected(this.props.value, option.props.value),
           variant: 'tag',
-          onMouseDown: (event) => { event.preventDefault(); this.downOption = option; },
+          onMouseDown: (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            this.downOption = option;
+          },
           onMouseUp: event => this.handleOptionClick(event, option),
           onMouseEnter: event => this.handleMouseEnter(event, option),
           ...(option.props.value === this.state.active) && { 'data-select-active': true },
