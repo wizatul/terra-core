03b56c379bbdfcab3163895acea5e262c72263f2
pranav agarwal <agarwal.pranav300@gmail.com>
2020-05-20 16:27:39 +0530

    example

--- packages/terra-button-group/src/terra-dev-site/test/button-group/ButtonGroupIsBlock.test.jsx
+++ packages/terra-button-group/src/terra-dev-site/test/button-group/ButtonGroupIsBlock.test.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
 import ButtonGroup from '../../../ButtonGroup';
 
+let returnButtonProps = {
+  text: 'Return',
+};
+const returnDisabledDefaultProps = () => {
+  returnButtonProps = {
+    isDisabled: true,
+    text: 'Disabled',
+  };
+};
+
+const returnBlurredDefaultProps = () => {
+  returnButtonProps = {
+    text: 'Blurred',
+  };
+};
+
 class ButtonGroupIsBlock extends React.Component {
   constructor(props) {
     super(props);
     this.state = { selectedKeys: [] };
     this.handleSelection = this.handleSelection.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
   handleSelection(event, key) {
     event.preventDefault();
     this.setState(prevState => ({ selectedKeys: ButtonGroup.Utils.handleMultiSelectedKeys(prevState.selectedKeys, key) }));
+  }
+
+  handleClick() {
+    returnDisabledDefaultProps();
+    this.forceUpdate();
+  }
+
+  handleBlur() {
+    returnBlurredDefaultProps();
+    this.forceUpdate();
   }
 
   render() {
@@ -22,7 +50,7 @@ class ButtonGroupIsBlock extends React.Component {
         selectedKeys={this.state.selectedKeys}
       >
         <ButtonGroup.Button text="Button 1" key="single-select1" />
-        <ButtonGroup.Button text="Button 2" key="single-select2" />
+        <ButtonGroup.Button text="Button 2" onClick={this.handleClick} onBlur={this.handleBlur} key="single-select2" {...returnButtonProps} />
         <ButtonGroup.Button text="Button 3" key="single-select3" />
       </ButtonGroup>
     );
